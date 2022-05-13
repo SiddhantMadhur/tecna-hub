5af0a1d5645a60af01a1ba856f7b19f50b64c17f
SiddhantMadhur <siddhant.madhur@gmail.com>
2022-05-13 17:26:54 +0400

    updated readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Tecna Hub
+### Developed by Siddhant Madhur
+Tecna is an organization dedicated to creating free, open-sourced, fully featured websites and desktop applications.
+
+**TecnaHUB** is the official site for news, downloads and updates from Tecna.
+
+Our current list of applications:
+- [Reminders+](https://github.com/SiddhantMadhur/remindersplus)
+- [Sids Password Manager](https://github.com/SiddhantMadhur/passwordmanager)
+
+Currently working on:
+- [TecnaFinanceManager](https://github.com/SiddhantMadhur/tecna)
+
+
--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,24 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>TecnaHub</title>
       </Head>
+      <div class="alert alert-warning shadow-lg">
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="stroke-current flex-shrink-0 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+            />
+          </svg>
+          <span>Warning: The site is a work in progress. Please visit later. <a href="https://github.com/SiddhantMadhur/tecna-hub">Visit the official github to stay updated.</a> </span>
+        </div>
+      </div>
       <Navbar />
       <body>
         <Component {...pageProps} />
